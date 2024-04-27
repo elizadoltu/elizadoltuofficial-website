1996977094c95cc071e6d96deb6439bca7542eca
elizadoltu <elizadoltuofficial@gmail.com>
2024-04-27 20:51:16 +0300

    solve:gsap animation

--- src/pages/LandingNewPage.jsx
+++ src/pages/LandingNewPage.jsx
@@ -5,37 +5,8 @@ import { gsap } from "gsap";
 
 const LandingPage = ({ scrollTo }) => {
   const cursorRef = useRef(null);
-  const buttonRefs = useRef([]);
 
   useEffect(() => {
-    buttonRefs.current.forEach((button) => {
-      button.addEventListener("mouseenter", () => {
-        gsap.to(button, {
-          y: -2,
-          duration: 0.2,
-          ease: "power2.out",
-        });
-        gsap.to(button.querySelector(".underline"), {
-          scaleX: 1,
-          duration: 0.2,
-          ease: "power2.out",
-        }); // Add underline animation
-      });
-
-      button.addEventListener("mouseleave", () => {
-        gsap.to(button, {
-          y: 0,
-          duration: 0.2,
-          ease: "power2.out",
-        });
-        gsap.to(button.querySelector(".underline"), {
-          scaleX: 0,
-          duration: 0.2,
-          ease: "power2.out",
-        }); // Remove underline animation
-      });
-    });
-
     const moveCursor = (e) => {
       cursorRef.current.style.left = e.pageX + "px";
       cursorRef.current.style.top = e.pageY + "px";
@@ -48,38 +19,78 @@ const LandingPage = ({ scrollTo }) => {
     };
   }, []);
 
+  const handleMouseEnter = (underline) => {
+    gsap.to(underline, {
+      scaleX: 1,
+      duration: 0.2,
+      ease: "power2.out",
+    });
+  };
+
+  const handleMouseLeave = (underline) => {
+    gsap.to(underline, {
+      scaleX: 0,
+      duration: 0.2,
+      ease: "power2.out",
+    });
+  };
+
   return (
     <div
       id="landing-page"
       className="w-full h-screen overflow-hidden bg-black font-urbanist text-font relative mb-20"
     >
       <CustomCursor ref={cursorRef} />
-      <div
-        ref={(el) => buttonRefs.current.push(el)}
-        className="flex flex-col justify-end items-end text-xl mt-10 mr-10"
-      >
+      <div className="flex flex-col justify-end items-end text-xl mt-10 mr-10">
         <div
           onClick={() => scrollTo("about-me-page")}
           className="cursor-pointer relative"
         >
-          about.
-          <div className="underline absolute bottom-0 left-0 bg-white w-full h-0.5 transform origin-left scaleX-0"></div>{" "}
+          <span className="text-button">about.</span>
+          <div
+            className="underline absolute bottom-0 left-0 bg-white w-full h-0.5 transform origin-left scaleX-0"
+            onMouseEnter={() =>
+              handleMouseEnter(document.getElementById("about-underline"))
+            }
+            onMouseLeave={() =>
+              handleMouseLeave(document.getElementById("about-underline"))
+            }
+            id="about-underline"
+          ></div>{" "}
           {/* Underline element */}
         </div>
         <div
           onClick={() => scrollTo("works-page")}
           className="cursor-pointer relative"
         >
-          works.
-          <div className="underline absolute bottom-0 left-0 bg-white w-full h-0.5 transform origin-left scaleX-0"></div>{" "}
+          <span className="text-button">works.</span>
+          <div
+            className="underline absolute bottom-0 left-0 bg-white w-full h-0.5 transform origin-left scaleX-0"
+            onMouseEnter={() =>
+              handleMouseEnter(document.getElementById("works-underline"))
+            }
+            onMouseLeave={() =>
+              handleMouseLeave(document.getElementById("works-underline"))
+            }
+            id="works-underline"
+          ></div>{" "}
           {/* Underline element */}
         </div>
         <div
           onClick={() => scrollTo("contact-page")}
           className="cursor-pointer relative"
         >
-          contact.
-          <div className="underline absolute bottom-0 left-0 bg-white w-full h-0.5 transform origin-left scaleX-0"></div>{" "}
+          <span className="text-button">contact.</span>
+          <div
+            className="underline absolute bottom-0 left-0 bg-white w-full h-0.5 transform origin-left scaleX-0"
+            onMouseEnter={() =>
+              handleMouseEnter(document.getElementById("contact-underline"))
+            }
+            onMouseLeave={() =>
+              handleMouseLeave(document.getElementById("contact-underline"))
+            }
+            id="contact-underline"
+          ></div>{" "}
           {/* Underline element */}
         </div>
       </div>
