cee49fcf8d42478fd938a44e6ed873d5e61bba80
elizadoltu <elizadoltuofficial@gmail.com>
2024-04-27 16:50:12 +0300

    add:gsap animations

--- src/pages/MyWorks.jsx
+++ src/pages/MyWorks.jsx
@@ -6,28 +6,30 @@ import WorkCard from "../components/WorkCard";
 import data from "../data/workData.js";
 
 const MyWorks = () => {
-  const workCardsRef = useRef([]);
+  const workCardsContainerRef = useRef(null);
 
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
 
-    workCardsRef.current.forEach((card, index) => {
-      gsap.to(card, {
-        y: () => `+=${index * 200}`, // Adjust the parallax effect as needed
-        scrollTrigger: {
-          trigger: card,
-          start: "top bottom",
-          end: "bottom top",
-          scrub: true, // Smoothly animates the element as you scroll
-        },
-      });
+    const workCards = workCardsContainerRef.current.children;
+
+    gsap.to(workCards, {
+      opacity: 1,
+      duration: 1,
+      ease: "power2.inOut",
+      scrollTrigger: {
+        trigger: workCardsContainerRef.current,
+        start: "top 80%", // Start fading in when the top of the container reaches 80% of the viewport height
+        end: "bottom 20%", // End fading in when the bottom of the container reaches 20% of the viewport height
+        scrub: true,
+      },
     });
   }, []);
 
   return (
     <div
       id="works-page"
-      className="w-full bg-background text-font font-urbanist overflow-x-hidden overflow-y-hidden h-auto -z-10"
+      className="w-full bg-background text-font font-urbanist overflow-x-hidden h-auto -z-10"
     >
       <div className="flex justify-center mt-16">
         <h1 className="font-mera text-10xl">PORTFOLIO</h1>
@@ -39,23 +41,28 @@ const MyWorks = () => {
         </div>
         <div className="bg-font w-24 h-1px ml-5"> </div>
       </div>
-      {data.map((work, index) => (
-        <div
-          ref={(element) => (workCardsRef.current[index] = element)}
-          key={work.index}
-        >
-          <WorkCard
-            index={work.index}
-            name={work.name}
-            picture={work.picture}
-            year={work.year}
-            month={work.month}
-            description={work.description}
-            skills={work.skills}
-            link={work.link}
-          />
-        </div>
-      ))}
+      <div
+        ref={workCardsContainerRef}
+        style={{ display: "flex", flexDirection: "column" }} // Ensure the container displays its children in a column layout
+      >
+        {data.map((work, index) => (
+          <div
+            key={work.index}
+            style={{ marginBottom: "100px", opacity: 0 }} // Initially set opacity to 0 for the fade-in effect
+          >
+            <WorkCard
+              index={work.index}
+              name={work.name}
+              picture={work.picture}
+              year={work.year}
+              month={work.month}
+              description={work.description}
+              skills={work.skills}
+              link={work.link}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
